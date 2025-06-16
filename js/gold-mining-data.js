66766b1b3c1ef0e4404db24fdb5b41361cbc62a4
dieseleagle <216467729+dieseleagle@users.noreply.github.com>
2025-06-16 18:48:28 -0300

    Fix map display issue by removing duplicate script loading

--- js/gold-mining-data.js
+++ js/gold-mining-data.js
@@ -2,14 +2,6 @@
 // Combined dataset of all gold mining locations in Nova Scotia
 // Focusing on historical, abandoned, and active gold mining sites
 
-// Import individual data parts
-document.write('<script src="js/gold-mining-data-part1.js"></script>');
-document.write('<script src="js/gold-mining-data-part2.js"></script>');
-document.write('<script src="js/gold-mining-data-part3.js"></script>');
-document.write('<script src="js/gold-mining-data-part4.js"></script>');
-document.write('<script src="js/gold-panning-locations.js"></script>');
-document.write('<script src="js/additional-gold-panning-locations.js"></script>');
-
 // Combine all data parts into a single array
 const allGoldLocations = [
     ...goldMiningDataPart1,
